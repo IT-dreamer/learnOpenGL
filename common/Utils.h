81ad345646908788f102a27c35baf6f1c7b5e0de
AFei <tjtgcyf@gmail.com>
2023-04-28 17:42:24 +0900

    update: cancel the global namespace of string

--- common/Utils.h
+++ common/Utils.h
@@ -7,9 +7,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-using std::string;
-
-string readShaderSource(const char *filePath);
+std::string readShaderSource(const char *filePath);
 GLuint creatShaderProgram(const char *vertexFile, const char *fragmentFile);
 GLFWwindow *openglInit(int window_width, int window_height, const char *window_name, GLFWmonitor *monitor, GLFWwindow *share);
 void printShaderLog(GLuint shader);
