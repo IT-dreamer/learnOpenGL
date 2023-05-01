8bc30d644332ed175515025ba0e769211093c098
IT-dreamer <tjtgcyf@gmail.com>
2023-05-01 16:46:57 +0900

    texture

--- Getting Started/texture.cpp
+++ Getting Started/texture.cpp
@@ -10,7 +10,7 @@ int main(void)
     auto window = openglInit(_WINDOW_WIDTH_, _WINDOW_HEIGHT_, "shader", nullptr, nullptr);
 
     //set the render program
-    auto program = creatShaderProgram("../shader/shader.vert", "../shader/shader.frag");
+    auto program = creatShaderProgram("../shader/texture.vert", "../shader/texture.frag");
 
     //data
     float vertices[] = {
@@ -21,7 +21,102 @@ int main(void)
     -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
     };
 
+    unsigned int indices[] = {
+    0, 1, 3, // triangleA
+    1, 2, 3  // triangleB
+    };
 
 
+    //set the vao vbo and ebo
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    //bind the vbo to vao
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //bind the ebo to vao
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //set the Attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    //generate texture object
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    //Set the surround and filter methods for the currently bound texture object
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    //load target texture 
+    const char *texture_path = "../texture/container.jpg";
+    int width, height;
+    auto image = SOIL_load_image(texture_path, &width, &height, 0, SOIL_LOAD_RGB);
+
+    if(image)
+    {
+        //bind the target texture to object
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        //unbind current texture
+        SOIL_free_image_data(image);
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+    else
+    {
+        std::cerr << "Failed to load texture: " << texture_path;
+        std::cerr << std::endl; 
+    }
+
+    //unbind the vao
+    glBindVertexArray(0);
+    
+    //rend loop
+    while(!glfwWindowShouldClose(window))
+    {
+        //set the background color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //set which shader program used
+        glUseProgram(program);
+
+        //set which texture used
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE0);
+        glUniform1i(glGetUniformLocation(program, "texture1"), 0);
+
+        //set which vao rendered
+        glBindVertexArray(vao);
+
+        //render
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        //swap buffer
+        glfwSwapBuffers(window);
+
+        //update window's status
+        glfwPollEvents();        
+    }
+
+    glfwDestroyWindow(window);
+    glDeleteBuffers(1, &ebo);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &vao);
+    glfwTerminate();
     return 0;
 }
