81fa0b26c68e54df3ebbd46882ddf6c6779600db
AFei <tjtgcyf@gmail.com>
2023-05-10 16:41:57 +0900

    camera

---
+++ Getting Started/camera.cpp
@@ -0,0 +1,252 @@
+#include "../common/Utils.h"
+#include <SOIL2/SOIL2.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#define _WINDOW_WIDTH_      600
+#define _WINDOW_HEIGHT_     600
+
+glm::mat4 myTransform(int screen_width, int screen_height);
+glm::mat4 myTransform(int screen_width, int screen_height, int i);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+static glm::vec3 cubePositions[] = 
+{
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+};
+
+// camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
+// other factor
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main(void)
+{
+    //init opengl and create window
+    auto window = openglInit(_WINDOW_WIDTH_, _WINDOW_HEIGHT_, "shader", nullptr, nullptr);
+
+    //set the render program
+    auto program = creatShaderProgram("../shader/camera.vert", "../shader/camera.frag");
+
+    //data
+    float vertices[] = {
+    //---- 位置 ----      - 纹理坐标 -
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    //set the vao vbo and ebo
+    GLuint vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    //bind the vbo to vao
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //set the Attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //generate texture1 object
+    GLuint texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    //Set the surround and filter methods for the currently bound texture object
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    //load target texture 
+    const char *texture1_path = "../texture/container.jpg";
+    texture2DRGBBind(texture1, texture1_path);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    //generate texture2 object
+    GLuint texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    //Set the surround and filter methods for the currently bound texture object
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    //load target texture
+    const char *texture2_path = "../texture/awesomeface.png";
+    texture2DRGBBind(texture2, texture2_path);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    //configure global opengl state
+    glEnable(GL_DEPTH_TEST);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //unbind the vao
+    glBindVertexArray(0);
+    
+    //rend loop
+    while(!glfwWindowShouldClose(window))
+    {
+        //set the background color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //set which shader program used
+        glUseProgram(program);
+
+        //set texture1
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glUniform1i(glGetUniformLocation(program, "texture1"), 0);
+
+        //set texture2
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(program, "texture2"), 1);
+        
+        //set which vao rendered
+        glBindVertexArray(vao);
+
+        //input
+        processInput(window);
+
+        for(int i = 0; i < 10; i++)
+        {
+            auto trans = myTransform(_WINDOW_WIDTH_, _WINDOW_HEIGHT_, i);
+            GLuint transformLoc = glGetUniformLocation(program, "transform");
+            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+            glDrawArrays(GL_TRIANGLES, 0, 36);                 
+        }
+
+        //swap buffer
+        glfwSwapBuffers(window);
+
+        //update window's status
+        glfwPollEvents();        
+    }
+
+    glfwDestroyWindow(window);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &vao);
+    glfwTerminate();
+    return 0;
+}
+
+glm::mat4 myTransform(int screen_width, int screen_height)
+{
+
+    float fov = (float)(screen_width / screen_height);
+    glm::mat4 transform(1.0f);
+    glm::mat4 mTransform = glm::translate(transform, glm::vec3(0.0f, 0.2f, 0.0f));
+    mTransform = glm::rotate(mTransform, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+    glm::mat4 vTransform = glm::translate(transform, glm::vec3(-0.3f, 0.0f, -5.0f));
+    glm::mat4 pTransform = glm::perspective(glm::radians(45.0f), fov, 0.1f, 100.0f);
+    transform = pTransform * vTransform * mTransform;
+    
+    return transform;
+}
+
+glm::mat4 myTransform(int screen_width, int screen_height, int i)
+{
+
+    float fov = (float)(screen_width / screen_height);
+    //float radius = 10.0f;
+    //float camX = sin(glfwGetTime()) * radius;
+    //float camZ = cos(glfwGetTime()) * radius;
+    glm::mat4 transform(1.0f);
+    glm::mat4 mTransform = glm::translate(transform, cubePositions[i]);
+    float angle = (float)glfwGetTime() * (i + 1) * 20.0f;
+    mTransform = glm::rotate(mTransform, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+    glm::mat4 vTransform = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    glm::mat4 pTransform = glm::perspective(glm::radians(45.0f), fov, 0.1f, 100.0f);
+    transform = pTransform * vTransform * mTransform;
+    
+    return transform;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float currentTime = glfwGetTime();
+    deltaTime = currentTime - lastFrame;
+    lastFrame = currentTime;
+    float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
